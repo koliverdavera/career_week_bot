b44f657a192e449e16466f44edea197412b9c2bb
Vera Koliverda <koliverdavera@gmail.com>
2021-11-16 06:15:20 +0300

    it's ready to be launched

--- create_database.py
+++ create_database.py
@@ -2,7 +2,11 @@ from data.events import Event
 from data.students import Student
 from data.companies import Company
 from data.database import create_db
+import click
+from flask.cli import with_appcontext
 
 
+@click.command(name='create_tables')
+@with_appcontext
 def create_database():
     create_db()
